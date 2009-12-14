9caa25eab2ae539783c0306d00c7d29be145f78a
Roman Smirnov <roman@railsdog.com>
2009-12-14 16:09:46 +0300

    Removed unused files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # Put your extension routes here.
 
-map.resources :products, :member => { :tell_a_friend => :get, :mail_to_friend => :put }
-
 map.email_to_friend "/email_to_friend/:type/:id", :controller => 'email_sender', :action => 'send_mail'
 
 map.namespace :admin do |admin|
