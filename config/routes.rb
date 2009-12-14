019582c469614f0f3265f71bb808019100702808
Roman Smirnov <roman@railsdog.com>
2009-12-14 16:07:20 +0300

    Refactoring of extension: for now we can provide to user oportunity to send link to any object, not only to product. like this: email_to_friend_url('product', @product) email_to_friend_url('wishlist', @wishlist)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 map.resources :products, :member => { :tell_a_friend => :get, :mail_to_friend => :put }
 
+map.email_to_friend "/email_to_friend/:type/:id", :controller => 'email_sender', :action => 'send_mail'
+
 map.namespace :admin do |admin|
   admin.resource :captcha_settings
 end
